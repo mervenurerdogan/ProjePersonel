0aaff364ef81f40074b889b21c2740d1022873e8
mervenurerdogan <merveeenur2@gmail.com>
2023-04-18 05:48:14 +0300

    Services MVC katmanına eklendi

---
+++ ProjePersonelBusiness/ServiceCollectionExtensions.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using ProjePersonelBusiness.Abstract;
+using ProjePersonelBusiness.Concrete;
+using ProjePersonelDataAccess.Abstract;
+using ProjePersonelDataAccess.Concrete;
+using ProjePersonelDataAccess.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjePersonelBusiness
+{
+    public static class ServiceCollectionExtensions
+    {
+
+
+        public static IServiceCollection LoadMyServices(this IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddDbContext<ProjePersonelContext>();
+   
+            serviceCollection.AddTransient<IDepartmanService, DepartmanManager>();
+            serviceCollection.AddTransient<IPersonnelService, PersonnelManager>();
+            serviceCollection.AddTransient<IPersonnelAddressService, PersonnelAddressManager>();
+            serviceCollection.AddTransient<IPersonnelPhoneNumberService, PhoneNumberManager>();
+            serviceCollection.AddTransient<IMissionService, MissionManager>();
+
+
+
+            serviceCollection.AddScoped<IMissionRepository, EfMissionRepository>();
+            serviceCollection.AddScoped<IPersonnelRepository, EfPersonnelRepository>();
+            serviceCollection.AddScoped<IPersonnelNumberRepository, EfPersonnelPhoneNumberRepository>();
+            serviceCollection.AddScoped<IPersonnelAddressRepository, EfPersonnelAddressRepository>();
+            serviceCollection.AddScoped<IDepartmanRepsitory, EfDepartmanRepository>();
+
+
+
+            return serviceCollection;
+
+        }
+    }
+}
--- ProjePersonelMVC/Program.cs
+++ ProjePersonelMVC/Program.cs
@@ -2,6 +2,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.LoadMyServices();
 
 var app = builder.Build();
 
